d9cd98c4191499c2bf043023711d094e7da311c4
Arkady Rosenberg <intelligent002@gmail.com>
2024-12-31 11:44:38 +0200

    dont show help always

--- ansible-kafka-backup/run.ps1
+++ ansible-kafka-backup/run.ps1
@@ -1,4 +1,5 @@
 alias ansible-playbook="docker run -ti --rm -v ~/.ssh:/root/.ssh -v $(pwd):/apps -v /data:/data -v /backup:/backup -w /apps alpine/ansible ansible-playbook"
+alias ansible-playbook="docker run -ti --rm -v ~/.ssh:/root/.ssh -v $(pwd):/apps -v /data:/data -v /backup:/backup -w /apps ansible-with-archivators ansible-playbook"
 
 ansible-playbook -i inventories/productions/inventory playbooks/playbook.yml --tags "backup_config"
 
