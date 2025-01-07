2fda80dd7110174495c03056380ed42408d93dca
Arkady Rosenberg <intelligent002@gmail.com>
2025-01-07 11:29:54 +0200

    all good !

--- ansible-kafka-backup/run.ps1
+++ ansible-kafka-backup/run.ps1
@@ -9,21 +9,23 @@ docker run -ti --rm `
     -w "/apps" `
     alpine/ansible ansible-playbook -i inventory playbook.yml --tags "config_backup"
 
-ansible-playbook -i inventories/kafka-6-vms/hosts.yml playbooks/parallel.yml --tags "certificate_backup"
 ansible-playbook -i inventories/kafka-6-vms/hosts.yml playbooks/parallel.yml --tags "certificate_generate"
-ansible-playbook -i inventories/kafka-6-vms/hosts.yml playbooks/parallel.yml --tags "certificate_restore" --extra-vars "restore_archive=/backup/cold/certificate/rotated/2025/01/06/2025-01-06---23-32-02---certificate.tar.zx"
+ansible-playbook -i inventories/kafka-6-vms/hosts.yml playbooks/parallel.yml --tags "certificate_backup"
+ansible-playbook -i inventories/kafka-6-vms/hosts.yml playbooks/parallel.yml --tags "certificate_rotate"
+ansible-playbook -i inventories/kafka-6-vms/hosts.yml playbooks/parallel.yml --tags "certificate_restore" --extra-vars "restore_archive=/backup/cold/certificate/rotated/2025/01/07/2025-01-07---11-26-34---certificate.tar.zx"
+ansible-playbook -i inventories/kafka-6-vms/hosts.yml playbooks/parallel.yml --tags "config_deploy"
 ansible-playbook -i inventories/kafka-6-vms/hosts.yml playbooks/parallel.yml --tags "config_backup"
-ansible-playbook -i inventories/kafka-6-vms/hosts.yml playbooks/parallel.yml --tags "config_restore"      --extra-vars "restore_archive=/backup/cold/config/rotated/2025/01/06/2025-01-06---23-30-04---config.tar.zx"
 ansible-playbook -i inventories/kafka-6-vms/hosts.yml playbooks/parallel.yml --tags "config_rotate"
-ansible-playbook -i inventories/kafka-6-vms/hosts.yml playbooks/parallel.yml --tags "data_backup"
+ansible-playbook -i inventories/kafka-6-vms/hosts.yml playbooks/parallel.yml --tags "config_restore"      --extra-vars "restore_archive=/backup/cold/config/rotated/2025/01/07/2025-01-07---11-28-08---config.tar.zx"
 ansible-playbook -i inventories/kafka-6-vms/hosts.yml playbooks/parallel.yml --tags "data_format"
-ansible-playbook -i inventories/kafka-6-vms/hosts.yml playbooks/parallel.yml --tags "data_restore"        --extra-vars "restore_archive=/backup/cold/data/rotated/2025/01/07/2025-01-07---00-01-28---data.tar.zx"
+ansible-playbook -i inventories/kafka-6-vms/hosts.yml playbooks/parallel.yml --tags "data_backup"
 ansible-playbook -i inventories/kafka-6-vms/hosts.yml playbooks/parallel.yml --tags "data_rotate"
-ansible-playbook -i inventories/kafka-6-vms/hosts.yml playbooks/serial.yml --tags "containers_remove"
-ansible-playbook -i inventories/kafka-6-vms/hosts.yml playbooks/serial.yml --tags "containers_restart"
+ansible-playbook -i inventories/kafka-6-vms/hosts.yml playbooks/parallel.yml --tags "data_restore"        --extra-vars "restore_archive=/backup/cold/data/rotated/2025/01/07/2025-01-07---00-01-28---data.tar.zx"
 ansible-playbook -i inventories/kafka-6-vms/hosts.yml playbooks/serial.yml --tags "containers_run"
-ansible-playbook -i inventories/kafka-6-vms/hosts.yml playbooks/serial.yml --tags "containers_start"
+ansible-playbook -i inventories/kafka-6-vms/hosts.yml playbooks/serial.yml --tags "containers_restart"
 ansible-playbook -i inventories/kafka-6-vms/hosts.yml playbooks/serial.yml --tags "containers_stop"
+ansible-playbook -i inventories/kafka-6-vms/hosts.yml playbooks/serial.yml --tags "containers_start"
+ansible-playbook -i inventories/kafka-6-vms/hosts.yml playbooks/serial.yml --tags "containers_remove"
 
 
 
