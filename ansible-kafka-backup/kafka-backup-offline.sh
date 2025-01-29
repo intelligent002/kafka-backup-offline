48d59a4b7126805a4299ee57bc795852b7ba6492
Arkady Rosenberg <intelligent002@gmail.com>
2025-01-29 23:45:27 +0200

    comments

--- ansible-kafka-backup/kafka-backup-offline.sh
+++ ansible-kafka-backup/kafka-backup-offline.sh
@@ -217,7 +217,7 @@ function run_ansible_routine()
         -v /var/log/ansible:/var/log/ansible \
         -w /apps \
         alpine/ansible ansible-playbook \
-        -i inventories/$INVENTORY/hosts.yml playbooks/$playbook.yml \
+        -T -i inventories/$INVENTORY/hosts.yml playbooks/$playbook.yml \
         --tags \"$tag\" $extra_vars"
 
     # Execute the command
