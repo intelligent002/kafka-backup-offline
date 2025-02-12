43608c84d5df6d1f67b20e0428d3dcd2a9859248
Arkady Rosenberg <intelligent002@gmail.com>
2025-02-12 17:30:38 +0200

    comments

--- ansible-kafka-backup/cron.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-log=/var/log/kafka/cron.log
-echo "-------------------------------------$(date) - Running backup-------------------------------------" >> $log
-./kafka-backup-offline.sh cluster_backup >> $log 2>&1
