49a7d3933dcfc2eea59b0ab8cbd04e2f23bccce7
Arkady Rosenberg <intelligent002@gmail.com>
2025-02-13 19:34:52 +0200

    added attempts to ansible

--- kafka-backup-offline.sh
+++ kafka-backup-offline.sh
@@ -282,6 +282,7 @@ function run_ansible_routine()
 
     # Append extra_vars only if it's not empty
     [[ -n "$extra_vars" ]] && docker_command+=("$extra_vars")
+    echo $docker_command
 
     # Loop for a few attempts
     while [[ $attempt -le $max_attempts ]]; do
