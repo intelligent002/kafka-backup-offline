c13fa47a92a12f20882bad4a1421273237126850
Arkady Rosenberg <intelligent002@gmail.com>
2025-03-09 23:27:37 +0200

    logs

--- kafka-backup-offline.sh
+++ kafka-backup-offline.sh
@@ -334,7 +334,7 @@ function run_ansible_routine()
     # Prepare the Docker command as an array (to avoid eval issues)
     local docker_command=(
         docker run $docker_options
-        -v ~/.ssh:/root/.ssh
+        -v /data:/data
         -v "$SCRIPT_DIR":/apps
         -v /var/log/ansible:/var/log/ansible
         -w /apps
